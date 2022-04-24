52d3e616709c3d8bd17c90fae772470050eb0b87
TrandeLik <feoctistov1580@yandex.ru>
2022-04-24 16:39:07 +0300

    polyline from file

--- graphics_demo_app.py
+++ graphics_demo_app.py
@@ -31,6 +31,10 @@ class App(tk.Tk):
                                            variable=self.to_smooth, command=self.draw_polylines)
         self.need_polygon = tk.IntVar()
         self.lbl_vertexes_count = tk.Label(self.fr_menu, text="Количество вершин в \nупрощенной ломаной: ")
+        self.lbl_others = tk.Label(self.fr_menu, text="Другие опции")
+        self.file_name = tk.Entry(self.fr_menu)
+        self.btn_file = tk.Button(self.fr_menu, text="Ломаная из файла")
+        self.btn_file.configure(command=self.line_from_file)
 
         self.lbl_count.grid(row=0, column=0, sticky="ew", padx=5, pady=10)
         self.ent_count.grid(row=1, column=0, sticky="ew", padx=5, pady=10)
@@ -42,27 +46,55 @@ class App(tk.Tk):
         self.cb_simplify.grid(row=7, column=0, sticky="ew", padx=5, pady=10)
         self.cb_smoothing.grid(row=8, column=0, sticky="ew", padx=5, pady=10)
         self.lbl_vertexes_count.grid(row=11, column=0, sticky="ew", padx=5, pady=20)
-
+        self.lbl_others.grid(row=12, column=0, sticky="ew", padx=5, pady=5)
+        self.file_name.grid(row=14, column=0, sticky="ew", padx=5, pady=10)
+        self.btn_file.grid(row=15, column=0, sticky="ew", padx=5, pady=10)
         self.fr_menu.grid(row=0, column=0, sticky="ns")
         self.cvs_graphics.grid(row=0, column=1, sticky="nsew")
-
+        self.is_from_file = False
         self.main_line = None
         self.equidistant = None
         self.segmentation = None
         self.simplified = None
         self.smoothed = None
 
+    def line_from_file(self):
+        self.is_from_file = True
+        f = open(self.file_name.get(), 'r')
+        polyline_main = Polyline("black", 0, 3)
+        for line in f:
+            dot = line.split()
+            polyline_main.polyline.append([2 * float(dot[0]), 1550 - 2 * float(dot[1])])
+        polyline_main.elements_count = len(polyline_main.polyline)
+        self.main_line = polyline_main
+        self.run()
+
+    def fill_polyline(self):
+        if self.need_polygon.get() == 0:
+            self.draw_polylines()
+        else:
+            polygon = copy.deepcopy(self.main_line.polyline)
+            polygon.append([self.main_line.polyline[0][0],
+                            self.main_line.polyline[self.main_line.elements_count - 1][1]])
+            self.cvs_graphics.create_polygon(polygon, outline="black", fill="black")
+
     def run_generate_line(self, _):
+        self.is_from_file = False
+        self.run()
+
+    def run(self):
         self.to_draw_main.set(1)
         self.to_equidistant.set(0)
         self.to_make_segmentation.set(0)
         self.to_simplify.set(0)
         self.to_smooth.set(0)
-        self.need_polygon.set(0)
         self.cvs_graphics.delete("all")
-        self.main_line = generate_line(int(self.ent_count.get()))
+        if not self.is_from_file:
+            self.main_line = generate_line(int(self.ent_count.get()))
         self.equidistant = equidistant_polyline(self.main_line)
-        self.segmentation = make_segmentation(self.equidistant, params.N_INIT, params.N_P, params.N_S, params.F)
+        self.segmentation = make_segmentation(self.equidistant, params.N_INIT,
+                                              int(0.2 * self.equidistant.elements_count),
+                                              int(0.2 * self.equidistant.elements_count), params.F)
         self.simplified = simplify(copy.deepcopy(self.segmentation), params.C, params.k, params.m, params.c_h)
         self.smoothed = smoothed_polyline(self.simplified)
         self.lbl_vertexes_count["text"] = f"Количество вершин в \nупрощенной ломаной: {self.smoothed.elements_count}"
